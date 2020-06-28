093b2119272dad0d0978d59ffe75b1885c5270df
xuzhou <xuzhou@@nicetuan.net>
2020-06-28 19:08:28 +0800

    ss

--- tenement-api/src/main/java/com/tenement/api/po/test.java
+++ tenement-api/src/main/java/com/tenement/api/po/test.java
@@ -2,12 +2,11 @@ package com.tenement.api.po;
 
 
 
-import com.tenement.api.config.AddressUtils;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
